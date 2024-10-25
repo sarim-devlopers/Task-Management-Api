8fed2f57d86a3dbd25e6e74b255610c35441a575
sarim-developers <samhoonmairexz@gmail.com>
2024-10-25 15:55:38 +0530

    ok

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,30 +3,24 @@ const router = express.Router();
 const { 
     createTask, 
     getTaskById, 
-    getTasks,        // assuming getTasks is intended as filterTasks
+    getTasks,        
     updateTask, 
     deleteTask, 
-    toggleTaskStatus  // assuming toggleTaskStatus replaces markTaskComplete
+    toggleTaskStatus  
   } = require('../controllers/taskController');
   
 const authMiddleware = require('../middlewares/authMiddleware');
 
-// Create a task
 router.post('/', authMiddleware, createTask);
 
-// Get task by ID
 router.get('/:id', authMiddleware, getTaskById);
 
-// Get all tasks (with optional filtering)
 router.get('/', authMiddleware, getTasks);
 
-// Update a task
 router.put('/:id', authMiddleware, updateTask);
 
-// Delete a task
 router.delete('/:id', authMiddleware, deleteTask);
 
-// Mark task as complete
 router.put('/:id/complete', authMiddleware,  toggleTaskStatus);
 
 module.exports = router;
--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -10,22 +10,11 @@ const {
 } = require('../controllers/teamController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-// Create a new team
 router.post('/', authMiddleware, createTeam);
 
-// Get a team by ID
 router.get('/:id', authMiddleware, getTeamById); 
-
-// Get all teams
 router.get('/', authMiddleware, getAllTeams);
-
-// Add a member to a team
 router.put('/:id/addMember', authMiddleware, addMemberToTeam);
-
-// Remove a member from a team
 router.put('/:id/removeMember', authMiddleware, removeMemberFromTeam);
-
-// Update team information
 router.put('/:id', authMiddleware, updateTeam);
-
 module.exports = router;
